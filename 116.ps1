a83b366cc57369c3a804794a31be8624298038e7
Sil van Dijke <silvandijke@gmail.com>
2025-06-19 14:29:55 +0200

    Add files via upload

---
+++ 116.ps1
@@ -0,0 +1,9 @@
+{
+  "message": "Welkom bij de Cloud Hosting API!",
+  "instructions": "Begin met /customers om alle klanten te zien",
+  "available_endpoints": [
+    "/customers",
+    "/customers/{id}/servers",
+    "/servers/{id}"
+  ]
+}
